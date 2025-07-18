39bc6b7ff162ddada6a7ec5619c798365f91e340
observer04 <tripathysarojinee@gmail.com>
2025-07-18 05:42:58 +0530

    export output in nextjs config and add build script

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+};
+
+export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
